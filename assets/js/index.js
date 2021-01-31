8b94f71d0439cfb7923bf102abae036a4a6aa7ab
Abrham Tesfaye <se.abrham.tesfaye@gmail.com>
2021-01-31 15:33:04 +0300

    added the  evaluatePostfix function

--- assets/js/index.js
+++ assets/js/index.js
@@ -158,3 +158,48 @@ const infixToPostfix = function (input) {
     return result.trimEnd();
 }; //#end of infixToPostfix function;
 
+evaluatePostfix = function (e) {
+    //       const e = '3 4 2 * 1 5 - 2 3 ^ ^ / +';
+    const s = [],
+        tokens = e.split(' ');
+    for (const t of tokens) {
+        const n = Number(t);
+        if (!isNaN(n)) {
+            s.push(n);
+        } else {
+            if (s.length < 2) {
+                alert(`${t}: ${s}: insufficient operands.`);
+            }
+            const o2 = s.pop(),
+                o1 = s.pop();
+            switch (t) {
+                case '+':
+                    s.push(o1 + o2);
+                    break;
+                case '-':
+                    s.push(o1 - o2);
+                    break;
+                case '*':
+                    s.push(o1 * o2);
+                    break;
+                case '/':
+                    s.push(o1 / o2);
+                    break;
+                case '^':
+                    s.push(Math.pow(o1, o2));
+                    break;
+                default:
+                    throw new Error(`Unrecognized operator: [${t}]`);
+            }
+        }
+        //   console.log(`${t}: ${s}`);
+
+    }
+
+
+    if (s.length > 1) {
+        alert("operator shortage")
+    }
+
+    return s[0];
+}
